58884470950812608b9abcbc13fb98d5061f12ef
Aaricia <aaricia.van-oostrom@capgemini.com>
2017-11-20 14:08:11 +0100

    Form for adding employee

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Employee } from "../Employee";
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
+  display='none';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showForm() : void {
+    let x = document.getElementById("formAddEmployee");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  add() : void {
+
   }
 
 }
