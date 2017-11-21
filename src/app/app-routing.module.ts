730c4b0d7a01058c37d2df9111933bce224a032d
Aaricia <aaricia.van-oostrom@capgemini.com>
2017-11-21 11:23:09 +0100

    in between

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EmployeeComponent } from "./employee/employee.component";
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'employee', component: EmployeeComponent },
+  { path: 'detail/:id', component: EmployeeDetailComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { EmployeeService } from './employee.service';
-import { HttpClientModule }    from '@angular/common/http'; 
+import { HttpClientModule }    from '@angular/common/http';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
