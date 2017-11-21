57172b129d5895a8dfe455f755dc7b273f7b5833
Aaricia <aaricia.van-oostrom@capgemini.com>
2017-11-21 09:53:21 +0100

    je moeder

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -12,6 +12,8 @@ export class EmployeeComponent implements OnInit {
 
   display = "none";
   employees: Employee[] = [];
+  page: number;
+  nrPages: number;
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -30,7 +32,8 @@ export class EmployeeComponent implements OnInit {
 
   getEmployees() : void {
     this.employeeService.getEmployees().subscribe( e => this.employees = e.data);
-    
+    this.employeeService.getEmployees().subscribe( e => this.page = e.page);
+    this.employeeService.getEmployees().subscribe( e => this.nrPages = e.total_pages);
   }
 
   addEmployee(id : number, first_name: string, last_name: string, avatar: string) : void {
